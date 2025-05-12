441df64e08687d444cc60c9a37b71eade7dc32ff
Nicolas Mota <nicolas.mota2000@gmail.com>
2025-05-12 10:54:54 -0300

    finsh

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// document
+//   .querySelector(".profile-card")
+//   .addEventListener("mouseenter", function () {
+//     this.classList.add("hover");
+//   });
+
+// document
+//   .querySelector(".profile-card")
+//   .addEventListener("mouseleave", function () {
+//     this.classList.remove("hover");
+//   });
+
+
+// window.addEventListener("load", function () {
+//   document.querySelector(".profile-card").classList.add("active");
+// });
